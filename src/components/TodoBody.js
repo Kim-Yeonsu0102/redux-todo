4f8ed721b50b9b4c652ff3e66e689fb262d4f262
Kim Yeon Su <96866200+Kim-Yeonsu0102@users.noreply.github.com>
2023-05-24 10:14:13 +0900

    리팩토링02_Done
    리팩토링02_Done

--- src/components/TodoBody.js
+++ src/components/TodoBody.js
@@ -7,30 +7,21 @@ import "./../App.css";
 function TodoBody(props) {
 	const { todo } = useSelector((state) => state);
 	const dispatch = useDispatch();
+	const onChangeHandler = (id) => dispatch(checkTodo(id))
 
-	console.log(todo);
-
-	const onChangeHandler = (id) => {
-		dispatch(checkTodo(id));
-	};
 	return (
 		<div >
 			{todo.map((item, i) => (
 				<div key={item.id} className="todoBody">
-					<input id="check"type="checkbox" onChange={(id) => onChangeHandler(item.id)}/>
-					<label for="check"/>
-					{/* 체크박스를 누르면->체인지 이벤트가 발생하면 // 
+					<input id="check" type="checkbox" onChange={(id) => onChangeHandler(item.id)} />
+							{/* 체크박스를 누르면->체인지 이벤트가 발생하면 // 
 							store의 todo.isCheck에 접근 -> dispatch(checkTodo())
 							boolean 값을 변화시킴   */}
-					<span style={{ textDecorationLine: item.isCheck ? 'line-through' : 'none'}}>
+					<span style={{ textDecorationLine: item.isCheck ? 'line-through' : 'none' }}>
 						{item.contents}
 					</span>
 
-					<button
-						type="button"
-						onClick={() => {
-							dispatch(deleteTodo(item.id));
-						}}>
+					<button type="button" onClick={() => dispatch(deleteTodo(item.id))}>
 						삭제
 					</button>
 				</div>
